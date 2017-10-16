ef264def14d0e594f0ffa3ee4afb34cb95abdc44
Štěpán Škorpil <stopka@training-online.eu>
2017-10-16 22:13:48 +0200

    Changed way of curl request building in HttpClient

--- src/Stopka/OpenviduPhpClient/Http/HttpClient.php
+++ src/Stopka/OpenviduPhpClient/Http/HttpClient.php
@@ -10,32 +10,41 @@ namespace Stopka\OpenviduPhpClient\Http;
 
 
 class HttpClient {
-    /** @var  resource */
-    private $resource;
 
     /** @var  string */
     private $hostUrl;
 
-    public function __construct() {
-        $this->resource = curl_init();
+    /** @var array */
+    private $options = [];
+
+    /** @var array */
+    private $oneTimeOptions = [];
+
+    /**
+     * @param int $option
+     * @param callable|mixed $value
+     * @param bool $oneTime
+     */
+    private function setOption(int $option, $value, bool $oneTime = false) {
+        if ($oneTime) {
+            $this->oneTimeOptions[$option] = $value;
+            return;
+        }
+        $this->options[$option] = $value;
     }
 
     /**
      * @param bool $value
-     * @throws HttpClientException
      */
     public function disableSSLPeerVerification(bool $value = true): void {
-        $result = curl_setopt($this->resource, CURLOPT_SSL_VERIFYPEER, !$value);
-        $this->throwExceptionIfError($result);
+        $this->setOption(CURLOPT_SSL_VERIFYPEER, !$value);
     }
 
     /**
      * @param bool $value
-     * @throws HttpClientException
      */
     public function disableSSLHostVerification(bool $value = true): void {
-        $result = curl_setopt($this->resource, CURLOPT_SSL_VERIFYHOST, !$value);
-        $this->throwExceptionIfError($result);
+        $this->setOption(CURLOPT_SSL_VERIFYHOST, !$value);
     }
 
     /**
@@ -62,58 +71,68 @@ class HttpClient {
         }
     }
 
+    /**
+     * @param int $auth
+     */
     public function setAuth(int $auth): void {
-        $result = curl_setopt($this->resource, CURLOPT_HTTPAUTH, $auth);
-        $this->throwExceptionIfError($result);
+        $this->setOption(CURLOPT_HTTPAUTH, $auth);
     }
 
     /**
      * @param string $username
      * @param string $password
-     * @throws HttpClientException
      */
     public function setUserPassword(string $username, string $password): void {
-        $result = curl_setopt($this->resource, CURLOPT_USERPWD, "$username:$password");
-        $this->throwExceptionIfError($result);
-    }
-
-    /**
-     * @param bool $result
-     * @throws HttpClientException
-     */
-    private function throwExceptionIfError(bool $result): void {
-        if (!$result) {
-            throw new HttpClientException(curl_error($this->resource), curl_errno($this->resource));
-        }
-    }
-
-    public function post(string $url, ?array $data = null): HttpResponse {
-        $result = curl_setopt($this->resource, CURLOPT_URL, $this->getFullUrl($url));
-        $this->throwExceptionIfError($result);
-        $result = curl_setopt($this->resource, CURLOPT_RETURNTRANSFER, true);
-        $this->throwExceptionIfError($result);
-        $result = curl_setopt($this->resource, CURLOPT_POST, true);
-        $this->throwExceptionIfError($result);
-        if ($data) {
-            $result = curl_setopt($this->resource, CURLOPT_POSTFIELDS, $data);
-            $this->throwExceptionIfError($result);
-        }
-        $output = curl_exec($this->resource);
-        $status = curl_getinfo($this->resource, CURLINFO_HTTP_CODE);
-        $this->throwExceptionIfError($output);
-        return new HttpResponse($status, $output);
-    }
-
-    public function __destruct() {
-        curl_close($this->resource);
+        $this->setOption(CURLOPT_USERPWD, "$username:$password");
     }
 
     /**
      * @param array $values
      */
     public function setHeaders(array $values): void {
-        //TODO check if replaces settings
-        $result = curl_setopt($this->resource, CURLOPT_HTTPHEADER, $values);
-        $this->throwExceptionIfError($result);
+        $this->setOption(CURLOPT_HTTPHEADER, $values);
+    }
+
+    /**
+     * @param bool $result
+     * @param resource $ch
+     * @throws HttpClientException
+     */
+    private function throwExceptionIfError(bool $result, resource $ch): void {
+        if (!$result) {
+            throw new HttpClientException(curl_error($ch), curl_errno($ch));
+        }
+    }
+
+    /**
+     * @param resource $ch
+     * @throws HttpClientException
+     */
+    private function applyOptions(resource $ch): void {
+        $result = curl_setopt_array($ch, $this->options);
+        $this->throwExceptionIfError($result,$ch);
+        $result = curl_setopt_array($ch, $this->oneTimeOptions);
+        $this->throwExceptionIfError($result,$ch);
+        $this->oneTimeOptions = [];
+    }
+
+    public function post(string $url, ?array $data = null): HttpResponse {
+        $ch = curl_init($this->getFullUrl($url));
+        $this->applyOptions($ch);
+        $result = curl_setopt($ch, CURLOPT_URL, $this->getFullUrl($url));
+        $this->throwExceptionIfError($result,$ch);
+        $result = curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $this->throwExceptionIfError($result,$ch);
+        $result = curl_setopt($ch, CURLOPT_POST, true);
+        $this->throwExceptionIfError($result,$ch);
+        if ($data) {
+            $result = curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            $this->throwExceptionIfError($result,$ch);
+        }
+        $output = curl_exec($ch);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $this->throwExceptionIfError($output,$ch);
+        curl_close($ch);
+        return new HttpResponse($status, $output);
     }
 }
